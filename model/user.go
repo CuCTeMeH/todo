8817b7bfdda7df88e32abb3c53885a18029ff85c
CuCTeMeH <kutinchev87@gmail.com>
2020-06-19 20:56:13 +0300

    new seed and reorder

--- model/user.go
+++ model/user.go
@@ -7,10 +7,10 @@ import (
 type User struct {
 	ID        int        `gorm:"primary_key;column:id"`
 	UUID      string     `gorm:"column:uuid"`
-	Username  string     `gorm:"column:uuid"`
+	Username  string     `gorm:"column:username"`
+	Email     string     `gorm:"column:email"`
 	FirstName string     `gorm:"column:first_name"`
 	LastName  string     `gorm:"column:last_name"`
-	Email     string     `gorm:"column:email"`
 	CreatedAt time.Time  `gorm:"column:created_at"`
 	UpdatedAt time.Time  `gorm:"column:updated_at"`
 	DeletedAt *time.Time `gorm:"column:deleted_at"`
--- todo_seed.sql
+++ todo_seed.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.5-10.4.11-MariaDB)
 # Database: todo
-# Generation Time: 2020-06-19 14:41:25 +0000
+# Generation Time: 2020-06-19 17:54:31 +0000
 # ************************************************************
 
 
@@ -40,10 +40,10 @@ UNLOCK TABLES;
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
 
-INSERT INTO `tasks` (`id`, `uuid`, `user_id`, `name`, `description`, `status`, `deadline`, `created_at`, `updated_at`, `deleted_at`, `list_id`)
+INSERT INTO `tasks` (`id`, `uuid`, `user_id`, `list_id`, `name`, `description`, `status`, `deadline`, `created_at`, `updated_at`, `deleted_at`)
 VALUES
-	(1,'50fe094d-9f9d-46c6-904d-bda872648540',1,'Test Task Name',NULL,'active','2020-06-19 10:30:29','2020-06-19 10:30:29','2020-06-19 10:30:29',NULL,1),
-	(2,'d27bd3d4-5c10-4561-8927-04d1068b3fa8',1,'Test Task Name 2',NULL,'active','2020-06-19 10:30:29','2020-06-19 10:30:29','2020-06-19 10:30:29',NULL,1);
+	(1,'50fe094d-9f9d-46c6-904d-bda872648540',1,1,'Test Task Name',NULL,'active','2020-06-19 10:30:29','2020-06-19 10:30:29','2020-06-19 10:30:29',NULL),
+	(2,'d27bd3d4-5c10-4561-8927-04d1068b3fa8',1,1,'Test Task Name 2',NULL,'active','2020-06-19 10:30:29','2020-06-19 10:30:29','2020-06-19 10:30:29',NULL);
 
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -55,9 +55,9 @@ UNLOCK TABLES;
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 
-INSERT INTO `users` (`id`, `uuid`, `first_name`, `last_name`, `email`, `created_at`, `updated_at`, `deleted_at`)
+INSERT INTO `users` (`id`, `uuid`, `username`, `email`, `first_name`, `last_name`, `created_at`, `updated_at`, `deleted_at`)
 VALUES
-	(1,'a8c07e16-1bc7-4554-9131-3e24407e2dbc','First Name','Last Name','email@email.com','2020-06-19 10:31:31','2020-06-19 10:31:31',NULL);
+	(1,'a8c07e16-1bc7-4554-9131-3e24407e2dbc','test_username','email@email.com','First Name','Last Name','2020-06-19 10:31:31','2020-06-19 10:31:31',NULL);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
