41b914f3f1df3375761c18e5e61681496e8500ce
CuCTeMeH <kutinchev87@gmail.com>
2020-06-21 21:03:53 +0300

    styling

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	if config.Settings.GetBool("AUTO_MIGRATE") == true {
 		model.AutoMigrate()
 	}
+
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
